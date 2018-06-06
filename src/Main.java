ab03699242de3a82be22836773dea6c026c00b4a
licanchen <licanchen430@163.com>
2018-06-06 22:30:33 +0800

    folders added

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,7 @@ public class Main {
     public static void main(String args[]) {
         System.out.println("Congratulations!");
         System.out.println("This is Frost's branch.");
+        System.out.println("And, have fun!");
     }
 
 }
