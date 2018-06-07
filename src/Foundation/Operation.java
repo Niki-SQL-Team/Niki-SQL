8d31ea705e67d8496841e884b141bc9fc5c206cd
licanchen <licanchen430@163.com>
2018-06-07 18:00:18 +0800

    Foundation structure added.

---
+++ src/Foundation/Operation.java
@@ -0,0 +1,18 @@
+package Foundation;
+
+public enum Operation {
+
+    CreateTable,
+    DropTable,
+
+    CreateIndex,
+    DropIndex,
+
+    Select,
+    Insert,
+    Delete,
+
+    Quit,
+    ExecFile
+
+}
