2e3a9107eb625d7e4211193fd424132c002c4c59
Lv <1144532078@qq.com>
2018-06-08 21:46:54 +0800

    add some comments

---
+++ src/Index Manager/BPlusTree.java
@@ -0,0 +1,22 @@
+package IndexManager;
+
+import java.io.*;
+import java.util.Vector;
+import BufferManger.*;
+import CatalogManger.*;
+import RecordManger.*;
+
+//B+树
+public class BPlusTree{
+	//创建新的B+树到内存中
+	
+	//以已有的block还原B+树
+	
+	//插入
+	
+	//精确查找
+	
+	//范围查找
+	
+	//删除
+}
---
+++ src/Index Manager/IndexManager.java
@@ -0,0 +1,26 @@
+package IndexManager;
+
+import java.io.*;
+import java.util.Vector;
+import BufferManger.*;
+import CatalogManger.*;
+import RecordManger.*;
+
+//index管理
+public class IndexManager{
+	//创建索引
+	public boolean createIndex();
+	
+	//删除整个索引
+	public boolean	dropIndex();
+	
+	//等值查找
+	
+	//范围查找
+	
+	//插入新值
+	
+	
+	//删除一个值
+	
+}
